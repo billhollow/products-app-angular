e8d8ec33ea8741cd348be17af91595e4feecd944
billhollow <40575441+billhollow@users.noreply.github.com>
2024-05-15 12:52:54 -0500

    add basic structure angular-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
